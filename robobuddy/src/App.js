24a8a12b1c99f4c1c313c7a64b20a91e82a8112c
Ketan <ketantoo@gmail.com>
2019-12-25 12:39:11 +0530

    using fetch to get users data

--- robobuddy/src/App.js
+++ robobuddy/src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
+// import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ""
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        return response.json();
+      })
+      .then(users => {
+        this.setState({ robots: users });
+      });
   }
 
   onSearchChange = event => {
